ab4b743ee1c7d735eb06d1964de54f16c65277a7
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-11-30 17:12:43 -0800

    (progress) connect live data to project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,25 +1,37 @@
 import React from 'react';
 
-const Project = props => {
-  const { projects } = props;
-  /* TODO:
-    1) replace static data with live API data from prop.projects;
-    2) fix donate progress bar & flip map */
-  return projects.map(project => {
+const Project = ({ projects }) =>
+  /* TODO: fix donate progress bar & flip map */
+  projects.map(project => {
+    const pName = project[0];
+    const pUrl = project[1];
+    const pImg = project[2];
+    const pLoc = project[3];
+    const pDesc = project[4];
+    const pDonateLink = project[5];
+    const pDonateGoal = project[6];
+    const pDonateRaised = project[7];
+    const pDonateEnable = project[8];
+
     return (
+
       <article
         className="fly-card fly-project fly-flip-effect vertical"
         itemScope
         itemType="http://schema.org/DonateAction"
-        key={project[0]}
+        key={pName}
       >
         <div className="boxed flip-front">
           <a
             className="project-media js-wave"
-            href="project-details.html"
+            href={!pUrl ? null : pUrl}
             itemProp="url"
           >
-            <img src="images/temp/project-7.jpg" alt="" itemProp="image" />
+            <img
+              src={!pImg ? 'images/temp/project-7.jpg' : pImg}
+              alt="Current project"
+              itemProp="image"
+            />
             <span className="progress">
               <span className="progress-label">0%</span>
               <span className="progress-bar" />
@@ -27,49 +39,54 @@ const Project = props => {
           </a>
           <div className="project-content">
             <h3 className="project-title" itemProp="name">
-              <a href="project-details.html">{project[0]}</a>
+              <a href={!pUrl ? null : pUrl}>{pName}</a>
             </h3>
 
             <div className="project-location">
               <a href="#" className="flip-button" itemProp="location">
                 <i className="material-icons">location_on</i>
-                Mombasa, Kenya
+                {!pLoc ? 'Earth, Solar System' : pLoc}
               </a>
             </div>
 
             <div className="project-description" itemProp="description">
               <p>
-                Nam liber tempor cum soluta nobis eleifend option congue nihil
-                imperdiet doming id quod mazim placerat facer possim assum.
-                Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
-                consectissim posuere cubilia Curae Suspendisse at consectetur
-                massa.
+                {!pDesc
+                  ? 'Description coming soon. Thank you for your patience!'
+                  : pDesc}
               </p>
             </div>
             <div className="project-footer">
               <ul className="project-stats">
                 <li>
-                  <div className="label">raised</div>
-                  <div className="value" data-raised={12731} key={project[0]}>
-                    <sup>$</sup>
-                    12,731
+                  <div className="label">{!pDonateEnable ? '' : 'raised'}</div>
+                  <div
+                    className="value"
+                    data-raised={!pDonateRaised ? 0 : { pDonateRaised }}
+                  >
+                    <sup>{!pDonateEnable ? '' : '$'}</sup>
+                    {pDonateRaised === '' ? '' : pDonateRaised}
                   </div>
                 </li>
                 <li>
-                  <div className="label">goal</div>
-                  <div className="value" data-goal={22500} itemProp="target">
-                    <sup>$</sup>
-                    22,500
+                  <div className="label">{!pDonateEnable ? '' : 'goal'}</div>
+                  <div
+                    className="value"
+                    data-goal={!pDonateGoal ? 0 : { pDonateGoal }}
+                    itemProp="target"
+                  >
+                    <sup>{!pDonateEnable ? '' : '$'}</sup>
+                    {pDonateGoal === '' ? '' : pDonateGoal}
                   </div>
                 </li>
               </ul>
               <div className="project-buttons">
                 <a
-                  href="project-details.html#donateForm"
+                  href={!pDonateLink ? pUrl : pDonateLink}
                   className="btn btn-yellow js-wave"
                   itemProp="potentialAction"
                 >
-                  Donate
+                  {!pDonateEnable ? 'Details' : 'Donate'}
                 </a>
               </div>
             </div>
@@ -79,17 +96,16 @@ const Project = props => {
           <div className="card-map" data-placeholder="waiting for map">
             <div
               className="google-map"
-              data-map-zoom={14}
+              data-map-zoom={10}
               data-map-type="roadmap"
               data-map-style="UmmaCharity"
-              data-map-address="1998 Hulman Blvd, Speedway, IN, 46222"
+              data-map-address={!pLoc ? 'USA' : pLoc}
               data-map-marker="images/marker.png"
               data-map-marker-size="[31,46]"
               data-map-marker-anchor="[16,46]"
-            >
-              {}
-            </div>
+            />
           </div>
+          {/* TODO: add conditional for map/social after flip map fix; add real social links */}
           <ul className="card-social">
             <li>
               <a href="#" className="fa fa-facebook js-wave" />
@@ -100,14 +116,10 @@ const Project = props => {
             <li>
               <a href="#" className="fa fa-instagram js-wave" />
             </li>
-            <li>
-              <a href="#" className="fa fa-google js-wave" />
-            </li>
           </ul>
         </div>
       </article>
     );
   });
-};
 
 export default Project;
